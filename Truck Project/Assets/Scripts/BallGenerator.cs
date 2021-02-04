c189664eef39c1cd0bb0ca72c7b90251c27efd4e
Michael Harmon <mfh.scoobydoo@gmail.com>
2021-02-03 21:49:12 -0700

    Create Ball Generator Script

---
+++ Truck Project/Assets/Scripts/BallGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+  public GameObject ball;
+  IEnumerator Start()
+  {
+    while (true)
+    {
+      yield return new WaitForSeconds(0.5f);
+      Instantiate(ball);
+    }
+  }
+}
