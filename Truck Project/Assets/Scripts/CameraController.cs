9a86302d257f7bfeb042c7ea494d2f7cf629f6bb
Michael Harmon <mfh.scoobydoo@gmail.com>
2021-02-04 20:18:22 -0700

    Created Simple Camera Controller Script

--- Truck Project/Assets/Scripts/CameraController.cs
+++ Truck Project/Assets/Scripts/CameraController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public GameObject cameraTarget;
+		private Vector3 cameraOffset;
     // Start is called before the first frame update
     void Start()
     {
-        
+				cameraOffset = transform.position - cameraTarget.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+				transform.position = cameraTarget.transform.position + cameraOffset;
     }
 }
