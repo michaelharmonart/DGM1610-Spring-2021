85cb886d37b755a4fc22764d29b92f6a1cf37cc0
Michael Harmon <mfh.scoobydoo@gmail.com>
2021-02-04 21:25:39 -0700

    Significant Car Controller Improvements

--- Truck Project/Assets/Scripts/CarController.cs
+++ Truck Project/Assets/Scripts/CarController.cs
@@ -6,9 +6,12 @@ public class CarController : MonoBehaviour
 {
     public Rigidbody2D backWheel;
     public Rigidbody2D frontWheel;
-    public JointMotor2D motor;
+		public Rigidbody2D body;
+		public float speed;
+		public float rotationSpeed;
     private float movement;
     public float torque;
+		public float bodyTorque;
     public float fourWheelDriveAmmount;
 
     // Start is called before the first frame update
@@ -24,7 +27,15 @@ public class CarController : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        backWheel.AddTorque(-movement * torque * Time.fixedDeltaTime * 10);
-        frontWheel.AddTorque(-movement * torque * Time.fixedDeltaTime * fourWheelDriveAmmount * 10);
+				if(Mathf.Abs(backWheel.angularVelocity) < speed || Mathf.Abs(backWheel.angularVelocity - movement) < Mathf.Abs(backWheel.angularVelocity))
+				{
+				backWheel.AddTorque(-movement * torque * Time.fixedDeltaTime * 100);
+				frontWheel.AddTorque(-movement * torque * Time.fixedDeltaTime * fourWheelDriveAmmount * 100);
+				}
+				if(Mathf.Abs(body.angularVelocity) < rotationSpeed || Mathf.Abs(body.angularVelocity + movement) < Mathf.Abs(body.angularVelocity))
+				{
+				body.AddTorque(movement * bodyTorque * Time.fixedDeltaTime * 100);
+				}
+				Debug.Log(backWheel.angularVelocity);
     }
 }
