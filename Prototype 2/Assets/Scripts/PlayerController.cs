24652f59bddc5d6ff996b2de61b7280314a398dc
Michael Harmon <mfh.scoobydoo@gmail.com>
2021-03-24 22:34:46 -0600

    Change Project versions

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed;
+    public float xRange;
+
+    private float horizontalInput,verticalInput;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
