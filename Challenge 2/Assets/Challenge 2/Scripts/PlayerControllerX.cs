2da58a8f4ed5f556b1a65bdecd9b76776174fc0c
Michael Harmon <mfh.scoobydoo@gmail.com>
2021-03-25 09:28:52 -0600

    Finish Challenge 2

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float rechargeTime = 0.5f;
+
+    private float recharge;
+
+    // Update is called once per frame
+    void Update()
+    {
+        recharge -= Time.deltaTime; 
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && recharge <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            recharge = rechargeTime;
+        }
+    }
+}
