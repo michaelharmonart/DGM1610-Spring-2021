5b6073affb87a356123e1a1ffd7f0deea055bfa1
Michael Harmon <mfh.scoobydoo@gmail.com>
2021-03-24 23:00:26 -0600

    Version Issues Fixed, Create PlayerController

---
+++ Protoype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject player;
+    public float speed,xRange;
+
+    private float inputHorizontal, inputVertical;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        inputVertical = Input.GetAxis("Vertical");
+        inputHorizontal = Input.GetAxis("Horizontal");
+        player.transform.Translate(inputHorizontal * speed * Time.deltaTime,0,0);
+        if (player.transform.position.x > xRange)
+        {
+            player.transform.Translate(xRange - player.transform.position.x,0,0);
+        }
+        if (player.transform.position.x < -xRange)
+        {
+            player.transform.Translate(-xRange - player.transform.position.x,0,0);
+        }
+    }
+}
