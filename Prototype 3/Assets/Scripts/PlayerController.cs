203321ee7e6e09bcc787b6ce472b279c4c7b50e4
Michael Harmon <mfh.scoobydoo@gmail.com>
2021-03-26 10:00:11 -0600

    Finish SideScroller

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -8,20 +8,30 @@ public class PlayerController : MonoBehaviour
     public float gravityMultiplier = 2f;
     public bool isOnGroud = true;
     public BoolData gameOver;
+    public ParticleSystem explosionParticles;
+    public ParticleSystem dirtParticles;
+    public AudioClip jumpSound,deathSound;
 
     private Rigidbody playerRB;
+    private Animator playerAnim;
+    private AudioSource playerAudio;
 
     void Start()
     {
         gameOver.data = false;   
         playerRB = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityMultiplier;
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGroud)
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGroud && !gameOver.data)
         {
+            playerAnim.SetTrigger("Jump_trig");
+            playerAudio.PlayOneShot(jumpSound, 0.2f);
+            dirtParticles.Stop();
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGroud = false;
         }
@@ -32,9 +42,15 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGroud = true;
+            dirtParticles.Play();
         } else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver.data = true;
+            playerAudio.PlayOneShot(deathSound, 0.75f);
+            explosionParticles.Play();
+            dirtParticles.Stop();
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
             Debug.Log("Game Over!");
         }
 
--- Prototype 3/Assets/Scripts/SpawnManager.cs
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -6,25 +6,33 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] obstacles;
     public Vector3 spawnLocation;
-    public float startDelay,repeatRate;
+    public float startDelay,minRepeatRate,maxRepeatRate;
     public Quaternion objectRotation;
     public BoolData gameOver;
-    
+    private float spawnInterval = 1f,timer;
+
     void Start()
     {
+        timer = startDelay;
         objectRotation.Set(0f,0f,0f,0f);
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        //InvokeRepeating("SpawnObstacle", startDelay, spawnInterval);
     }
 
     void Update()
     {
-
+        if (timer <= 0)
+        {
+            SpawnObstacle();
+            timer = spawnInterval;
+        }
+        timer -= Time.deltaTime;
     }
 
     void SpawnObstacle()
     {
         if (!gameOver.data){
             Instantiate(obstacles[Random.Range(0,obstacles.Length)], spawnLocation, objectRotation);
+            spawnInterval = Random.Range(minRepeatRate,maxRepeatRate);
         }
     }
 }
