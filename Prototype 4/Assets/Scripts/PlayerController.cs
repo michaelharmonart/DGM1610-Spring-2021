8a0530197c6d9a9a8af0737ddcd7aec23603d211
Michael Harmon <mfh.scoobydoo@gmail.com>
2021-03-30 09:38:47 -0600

    Create Player Controller

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public GameObject focalPoint;
+
+    private Rigidbody playerRigidbody;
+    private float horizontalInput, verticalInput;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
+    }
+}
