41eb43a36ca49aba725ea96fae73ff7451fe9f86
Michael Harmon <mfh.scoobydoo@gmail.com>
2021-04-01 14:08:04 -0600

    Create Powerup

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
-    public GameObject focalPoint;
+    public GameObject focalPoint, powerupIndicator;
+    public bool hasPowerup = false;
+    public float powerupStrength = 10f;
+    public float powerupDuration = 5f;
 
     private Rigidbody playerRigidbody;
     private float horizontalInput, verticalInput;
@@ -19,5 +22,35 @@ public class PlayerController : MonoBehaviour
     {
         verticalInput = Input.GetAxis("Vertical");
         playerRigidbody.AddForce(focalPoint.transform.forward * verticalInput * speed);
+        powerupIndicator.transform.position = transform.position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCountdownRoutine());
+            Destroy(other.gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (other.gameObject.transform.position - transform.position);
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(powerupDuration);
+        powerupIndicator.SetActive(false);
+        hasPowerup = false;
     }
 }
