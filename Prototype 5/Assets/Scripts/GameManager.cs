41ab19936b4ae6c0534c781799f13062e24c230d
Michael Harmon <mfh.scoobydoo@gmail.com>
2021-04-06 15:54:22 -0600

    Spawn and Destroy Targets

---
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] targets;
+    private float spawnRate = 1;
+
+    void Start()
+    {
+        StartCoroutine("SpawnTarget");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            Instantiate(targets[Random.Range(0,targets.Length)]);
+        }
+    }
+}
---
+++ Prototype 5/Assets/Scripts/TargetBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBehaviour : MonoBehaviour
+{
+    private float maxForce = 14;
+    private float minForce = 8;
+    private float maxTorque = 3;
+    private float xRange = 6;
+    private float ySpawn = 0;
+
+
+    private Rigidbody targetRigidbody;
+    private Vector3 randomForce, randomPosition, randomTorque;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRigidbody = GetComponent<Rigidbody>();
+        randomForce.Set(0f,Random.Range(minForce,maxForce),0f);
+        randomTorque.Set(Random.Range(-maxTorque,maxTorque),Random.Range(-maxTorque,maxTorque),Random.Range(-maxTorque,maxTorque));
+        randomPosition.Set(Random.Range(-xRange,xRange),ySpawn,0f);
+        targetRigidbody.AddTorque(randomTorque, ForceMode.Impulse);
+        targetRigidbody.AddForce(randomForce, ForceMode.Impulse);
+        transform.position = randomPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter (Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
